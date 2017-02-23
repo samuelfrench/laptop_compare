54a9ae1d9ee78a2a21076aec5fd1a3d581db970f
Samuel French <samfrench@gmail.com>
2017-02-23 09:14:43 -0600

    add jdbc template deps2

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,11 +1,16 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
