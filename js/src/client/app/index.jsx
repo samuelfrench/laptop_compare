c6991a3a4bd95bce52519c0a91bd3f498b2e45b7
Samuel French <samfrench@gmail.com>
2017-03-03 14:06:42 -0600

    print data to screen

--- js/src/client/app/index.jsx
+++ js/src/client/app/index.jsx
@@ -4,16 +4,23 @@ import axios from 'axios';
 import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {tableData : "Loading..."};
+  }
+  
 //see https://daveceddia.com/ajax-requests-in-react/
 componentDidMount() {
     axios.get(`/data/laptop/`)
       .then(res => {
         console.log(res.data);
+        this.setState({tableData: res.data});
       });
   }
   render () {
     return (
 		<div>
+            <span>{this.state.tableData.toString()}</span>
 			<p> Hello React!23332</p>
 			<AwesomeComponent />
 		</div>
