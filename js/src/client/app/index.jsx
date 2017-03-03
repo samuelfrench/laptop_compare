832f0e5676b183f244d90532ef301c1b3d9b6ab0
Samuel French <samfrench@gmail.com>
2017-03-03 14:25:24 -0600

    print laptop data as table

--- js/src/client/app/index.jsx
+++ js/src/client/app/index.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import axios from 'axios';
 import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {tableData : "Loading..."};
-  }
-  
-//see https://daveceddia.com/ajax-requests-in-react/
-componentDidMount() {
-    axios.get(`/data/laptop/`)
-      .then(res => {
-        console.log(res.data);
-        this.setState({tableData: res.data});
-      });
-  }
-  render () {
-    return (
-		<div>
-            <span>{this.state.tableData.toString()}</span>
-			<p> Hello React!23332</p>
-			<AwesomeComponent />
-		</div>
-	);
-  }
+    constructor( props ) {
+        super( props );
+        this.state = { tableData: "Loading..." };
+    }
+
+    //see https://daveceddia.com/ajax-requests-in-react/
+    componentDidMount() {
+        axios.get( `/data/laptop/` )
+            .then( res => {
+                console.log( res.data );
+                this.setState( {
+                    tableData: res.data.map(( laptopItem ) =>
+                        <tr>
+                        <td>{laptopItem.model}</td>
+                        <td>{laptopItem.brand}</td>
+                        </tr>
+                    )
+                });
+            })
+    };
+    render() {
+        return (
+            <div>
+                <table>
+                <tr>
+                <th> Model # </th>
+                <th> Brand </th>
+                </tr>
+                {this.state.tableData}
+                </table>
+                <p> Hello React!23332</p>
+                <AwesomeComponent />
+            </div>
+        );
+    }
 }
 
-render(<App/>, document.getElementById('app'));
+render( <App />, document.getElementById( 'app' ) );
