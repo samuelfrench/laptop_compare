0c0f9faf5d07dc9308318c8294cd033c7e37cfeb
Samuel French <samfrench@gmail.com>
2017-03-03 16:55:38 -0600

    create laptop model

--- src/main/java/model/Laptop.java
+++ src/main/java/model/Laptop.java
@@ -1,32 +1,158 @@
 package model;
 
 public class Laptop {
-	private long id;
-	private String model, brand;
-	public Laptop(long id, String model, String brand) {
-		super();
-		this.id = id;
-		this.model = model;
-		this.brand = brand;
+	private String mfr;
+	private String osType;
+	private String osVersion;
+	private String cpuType;
+	private String cpuModel;
+	private String storageType;
+	private String displayRes;
+	private String laptopModel;
+	private String screenSize;
+	private String weight;
+	private String batteryLifeHr;
+	private String baseCpuClockGhz;
+	private String ramSizeGb;
+	private String storageSizeGb;
+	private String isTouchScreen;
+	private String isIpsScreen;
+	private String isTabConvert;
+	private String isDiscreteGpu;
+	private String releaseDate;
+	private String photoUrl;
+	private String price;
+	private String buyLink;
+	public String getMfr() {
+		return mfr;
 	}
-	public Laptop() {
+	public void setMfr(String mfr) {
+		this.mfr = mfr;
 	}
-	public long getId() {
-		return id;
+	public String getOsType() {
+		return osType;
 	}
-	public void setId(long id) {
-		this.id = id;
+	public void setOsType(String osType) {
+		this.osType = osType;
 	}
-	public String getModel() {
-		return model;
+	public String getOsVersion() {
+		return osVersion;
 	}
-	public void setModel(String model) {
-		this.model = model;
+	public void setOsVersion(String osVersion) {
+		this.osVersion = osVersion;
 	}
-	public String getBrand() {
-		return brand;
+	public String getCpuType() {
+		return cpuType;
 	}
-	public void setBrand(String brand) {
-		this.brand = brand;
+	public void setCpuType(String cpuType) {
+		this.cpuType = cpuType;
+	}
+	public String getCpuModel() {
+		return cpuModel;
+	}
+	public void setCpuModel(String cpuModel) {
+		this.cpuModel = cpuModel;
+	}
+	public String getStorageType() {
+		return storageType;
+	}
+	public void setStorageType(String storageType) {
+		this.storageType = storageType;
+	}
+	public String getDisplayRes() {
+		return displayRes;
+	}
+	public void setDisplayRes(String displayRes) {
+		this.displayRes = displayRes;
+	}
+	public String getLaptopModel() {
+		return laptopModel;
+	}
+	public void setLaptopModel(String laptopModel) {
+		this.laptopModel = laptopModel;
+	}
+	public String getScreenSize() {
+		return screenSize;
+	}
+	public void setScreenSize(String screenSize) {
+		this.screenSize = screenSize;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getBatteryLifeHr() {
+		return batteryLifeHr;
+	}
+	public void setBatteryLifeHr(String batteryLifeHr) {
+		this.batteryLifeHr = batteryLifeHr;
+	}
+	public String getBaseCpuClockGhz() {
+		return baseCpuClockGhz;
+	}
+	public void setBaseCpuClockGhz(String baseCpuClockGhz) {
+		this.baseCpuClockGhz = baseCpuClockGhz;
+	}
+	public String getRamSizeGb() {
+		return ramSizeGb;
+	}
+	public void setRamSizeGb(String ramSizeGb) {
+		this.ramSizeGb = ramSizeGb;
+	}
+	public String getStorageSizeGb() {
+		return storageSizeGb;
+	}
+	public void setStorageSizeGb(String storageSizeGb) {
+		this.storageSizeGb = storageSizeGb;
+	}
+	public String getIsTouchScreen() {
+		return isTouchScreen;
+	}
+	public void setIsTouchScreen(String isTouchScreen) {
+		this.isTouchScreen = isTouchScreen;
+	}
+	public String getIsIpsScreen() {
+		return isIpsScreen;
+	}
+	public void setIsIpsScreen(String isIpsScreen) {
+		this.isIpsScreen = isIpsScreen;
+	}
+	public String getIsTabConvert() {
+		return isTabConvert;
+	}
+	public void setIsTabConvert(String isTabConvert) {
+		this.isTabConvert = isTabConvert;
+	}
+	public String getIsDiscreteGpu() {
+		return isDiscreteGpu;
+	}
+	public void setIsDiscreteGpu(String isDiscreteGpu) {
+		this.isDiscreteGpu = isDiscreteGpu;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getBuyLink() {
+		return buyLink;
+	}
+	public void setBuyLink(String buyLink) {
+		this.buyLink = buyLink;
 	}
 }
