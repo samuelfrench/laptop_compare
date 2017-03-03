11ffe08f6a7c7798546a88ef83fdd54c1ddcdc50
Samuel French <samfrench@gmail.com>
2017-03-03 08:09:17 -0600

    install axios

--- js/src/client/app/index.jsx
+++ js/src/client/app/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
+import axios from 'axios';
 import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
   render () {
     return (
 		<div>
-			<p> Hello React!</p>
+			<p> Hello React!22</p>
 			<AwesomeComponent />
 		</div>
 	);
