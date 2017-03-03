134682e0835da2d6bf2ec4461f5439b9e3dd3bc9
Samuel French <samfrench@gmail.com>
2017-03-03 08:04:36 -0600

    fix build script typo

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src.main/resources/static');
+var BUILD_DIR = path.resolve(__dirname, '../src/main/resources/static');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
