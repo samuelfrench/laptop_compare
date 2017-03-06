d3866bfd5bf33a4ae08a9b0cf631d7e491af51ca
Samuel French <samfrench@gmail.com>
2017-03-06 14:55:27 -0600

    add tableheader object and dao

---
+++ src/main/java/com/french/dao/TableHeaderDAO.java
@@ -0,0 +1,9 @@
+package com.french.dao;
+
+import java.util.List;
+
+import com.french.model.TableHeader;
+
+public interface TableHeaderDAO {
+	public List<TableHeader> getAllTableHeaders();
+}
---
+++ src/main/java/com/french/model/TableHeader.java
@@ -0,0 +1,50 @@
+package com.french.model;
+
+public class TableHeader {
+	private String headerTypeCode;
+	private String headerText;
+	private String headerCode;
+	private Boolean isDefault;
+	private Integer sequence;
+
+	public TableHeader(String headerTypeCode, String headerText, String headerCode, Boolean isDefault,
+			Integer sequence) {
+		super();
+		this.headerTypeCode = headerTypeCode;
+		this.headerText = headerText;
+		this.headerCode = headerCode;
+		this.isDefault = isDefault;
+		this.sequence = sequence;
+	}
+	
+	public String getHeaderTypeCode() {
+		return headerTypeCode;
+	}
+	public void setHeaderTypeCode(String headerTypeCode) {
+		this.headerTypeCode = headerTypeCode;
+	}
+	public String getHeaderText() {
+		return headerText;
+	}
+	public void setHeaderText(String headerText) {
+		this.headerText = headerText;
+	}
+	public String getHeaderCode() {
+		return headerCode;
+	}
+	public void setHeaderCode(String headerCode) {
+		this.headerCode = headerCode;
+	}
+	public Boolean getIsDefault() {
+		return isDefault;
+	}
+	public void setIsDefault(Boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+	public Integer getSequence() {
+		return sequence;
+	}
+	public void setSequence(Integer sequence) {
+		this.sequence = sequence;
+	}
+}
