1412638433cbe2bbf1648d013aa25e44d5e39bb2
Samuel French <samfrench@gmail.com>
2017-03-15 05:52:48 +0800

    basic table working

--- js/src/client/app/index.jsx
+++ js/src/client/app/index.jsx
@@ -8,12 +8,23 @@ class App extends React.Component {
     constructor( props ) {
         super( props );
         this.state = { tableData: "Loading..." };
+        this.state.products = [{
+            id: 1,
+            name: "Product1",
+            price: 120
+        }, {
+            id: 2,
+            name: "Product2",
+            price: 80
+        }];
     }
     render() {
         return (
-            <div>
-                <MainLaptopTable />
-            </div>
+            <BootstrapTable data={this.state.products} striped hover>
+                <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
+                <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+                <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+            </BootstrapTable>
         );
     }
 }
