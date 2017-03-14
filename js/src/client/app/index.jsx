6f4c4e7ad9345c8f6ae48804d139ddbacf4ffc05
Samuel French <samfrench@gmail.com>
2017-03-15 05:48:36 +0800

    cleanup

--- js/src/client/app/index.jsx
+++ js/src/client/app/index.jsx
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import axios from 'axios';
 import MainLaptopTable from './MainLaptopTable.jsx';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-//import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
     constructor( props ) {
