282392e8d435d12be4c793bc3c4ae892371281c5
Samuel French <samfrench@gmail.com>
2017-03-15 06:15:13 +0800

    table is drawing
    styling is incorrect

--- js/src/client/app/index.jsx
+++ js/src/client/app/index.jsx
@@ -7,24 +7,12 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 class App extends React.Component {
     constructor( props ) {
         super( props );
-        this.state = { tableData: "Loading..." };
-        this.state.products = [{
-            id: 1,
-            name: "Product1",
-            price: 120
-        }, {
-            id: 2,
-            name: "Product2",
-            price: 80
-        }];
     }
     render() {
         return (
-            <BootstrapTable data={this.state.products} striped hover>
-                <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
-                <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-                <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-            </BootstrapTable>
+            <div>
+                <MainLaptopTable />
+            </div>
         );
     }
 }
